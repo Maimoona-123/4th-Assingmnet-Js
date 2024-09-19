b9759bd0e7944ad3581a51d393383d0ad6b24ca2
maimoona <maimoonaaltaf7@gmail.com>
2024-09-19 07:12:20 -0700

    new files and folder

---
+++ app.js
@@ -0,0 +1,85 @@
+// chap 12 - 13    ( IF...ELSE & ELSE IF STATEMENT,TESTING SET OF CONDITIONS )
+
+
+
+
+
+
+// Q-5
+
+// var password = prompt ( " Enter your password ...." );
+
+// if ( password === "shaz123" ) {
+//   console.log ( "Correct password ..!" );
+
+// } else {
+//   console.log  ( " Incorrect password ... "  ) ;
+// }
+
+
+
+
+// Q-6
+
+
+// var greeting;
+// var hour = 13;
+
+// if (hour < 18) {
+//     console.log ( greeting = "Good day" );
+// }
+
+// else  {
+//     console.log ( greeting = "Good evening" )
+// }
+
+
+
+
+// Q-7
+
+
+// var time = prompt ( "Enter a time 24 hours" )
+
+// if ( time >= 0 && time < 12 ) {
+//   console.log ( "Good morning ...!" ) 
+
+
+// } else if ( time >= 12 && time < 6 ) {
+//   console.log ( "Good aternoon ...!" )
+
+
+// } else if ( time >= 6 && time < 10 ) {
+//   console.log ( "Good night ...!" )
+
+// }  else if ( time >= 10 && time < 12 ) {
+//   console.log ( "Good night ...!" )
+
+// } else {
+//   console.log ( "Invalid ..." )
+// }
+
+
+
+
+// var time = +prompt("Enter a time for 24 hours");  
+
+// if ( time  <= 12 ) {  
+//   console.log ( "Good morning ...!" );
+
+
+// } else if ( time <= 5) {  
+//   console.log ( "Good afternoon ...!" );
+
+
+// } else if ( time <= 10 ) {  
+//   console.log ( "Good evening ...!" );
+
+
+// } else if ( time <= 1 ) {    
+//   console.log  ( "Good night ..!" );
+
+
+// } else {
+//     console.log ( " Invalid time " );  
+// }
